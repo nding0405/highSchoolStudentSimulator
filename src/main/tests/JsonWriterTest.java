6908c0e02a72a0a0f62a888308d51e9ed5090b8d
ningding <2488940755@qq.com>
2023-11-26 17:20:14 -0800

    Comment out unused classes and added UML diagram

--- src/main/tests/JsonWriterTest.java
+++ src/main/tests/JsonWriterTest.java
@@ -5,7 +5,6 @@ import model.Student;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import persistence.JsonReader;
-import persistence.JsonTest;
 import persistence.JsonWriter;
 
 import java.io.IOException;
